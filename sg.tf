1b31c61eff8bfbd18facb76ded78fc00f8ad46e9
rthootika <msamrat789@gmail.com>
2023-03-08 23:29:11 +0530

    Updates

--- sg.tf
+++ sg.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr, var.BASTION_NODE]
+    cidr_blocks = [var.vpc_cidr, "0.0.0.0/0"]
   }
 
   egress {
