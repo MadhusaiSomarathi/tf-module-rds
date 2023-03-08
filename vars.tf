207d02d77d65af04a21da63428ffd37763b22406
rthootika <msamrat789@gmail.com>
2023-03-08 13:46:25 +0530

    Updates

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "env" {}
+variable "subnets" {}
+variable "allocated_storage" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "skip_final_snapshot" {}
+variable "name" {}
+variable "vpc_id" {}
+variable "vpc_cidr" {}
+variable "BASTION_NODE" {}
+variable "nodes" {}
